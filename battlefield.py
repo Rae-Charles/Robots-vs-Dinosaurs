ceb0c5917c9cb59ce44b7aefbdd14c8c3fca4edf
Rachel Harris <rachelharris@MacBook-Air.local>
2021-10-14 16:29:58 -0500

    added scenario for running game with health of fleet and herd above 0, draft

--- battlefield.py
+++ battlefield.py
@@ -7,4 +7,14 @@ class Battlefield:
 
 
     def run_game(self):
-        pass
+        health = > 0
+        keep_running_scenario = True
+
+        while keep_running_scenario is True:
+            if health Fleet() == > 0 
+                run_game = True
+            elseif: 
+            if health Herd() == >0
+                rung_game = True
+            else: 
+                #stop running game     
--- main.py
+++ main.py
@@ -2,4 +2,3 @@ from battlefield import Battlefield
 battlefield = Battlefield()
 battlefield.run_game()
 
-#adding text to test commit
